c49800f2245cebb3896d42fa8b4b35402e05e655
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-16 22:09:07 +0530

    Merge changes from remote

--- backend-django/productsapi/admin.py
+++
@@ -1,8 +0,0 @@
-
-
-from django.contrib import admin
-from .models import Category,ClothProduct,ProductImage
-
-admin.site.register(Category)
-admin.site.register(ClothProduct)
-admin.site.register(ProductImage)
--- backend-django/productsapi/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import ClothProductListView
-
-urlpatterns = [
-    path('api/cloth_products/', ClothProductListView.as_view(), name='cloth-product-list'),
-    path('api/cloth_products/<str:id>/', ClothProductListView.as_view(), name='cloth-product-list'),
-    path('api/categories/<str:category>/', ClothProductListView.as_view(), name='category-cloth-product-list'),
-]
